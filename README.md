292dfc7b1fcfe28070de0f70291a3d5b74f1cc0d
Simon Bruder <simon@sbruder.de>
2019-08-04 23:08:18 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# mpv-matroska-crop
+
+This is a script for [mpv](https://mpv.io/) which automatically crops the video
+if the [Matroska PixelCrop
+properties](https://www.matroska.org/technical/specs/index.html#PixelCropBottom)
+are set.
+
+**NOTE: Cropping does not work with hardware decoding.** By default this script
+is only active when hardware decoding is disabled. See [Dynamic control of
+hardware decoding](#dynamic-control-of-hardware-decoding) for a workaround.
+
+## Installation
+
+The only dependency is `mkvmerge` from
+[mkvtoolnix](https://mkvtoolnix.download/).
+
+Copy `matroska_crop.lua` into the `scripts` directory in your mpv config
+directory (defaults to `~/.config/mpv/` on Linux). If the directory doesn’t
+exist, create it.
+
+## Dynamic control of hardware decoding
+
+Since cropping is not available with hardware decoding, this script can be
+configured to only be active if a crop is defined in the media file.
+
+For this behaviour to be active, create the file `matroska_crop.conf` in the
+`script-opts` directory in your mpv config directory and add the following
+contents:
+
+```
+dynamic_hwdec=yes
+```
+
+Please not the absence of spaces around the equal sign.
+
+## Drawbacks
+
+ * It cannot crop the video while using hardware decoding (limitation of mpv)
+ * It only works for local files (limitation of mkvmerge)  
+   This could be solved by mpv exposing the needed matroska metadata
+ * There might be an edge case I haven’t tested where it doesn’t work
---
+++ matroska_crop.lua
@@ -0,0 +1,112 @@
+local msg = require 'mp.msg'
+local utils = require 'mp.utils'
+require 'mp.options'
+
+-- HELPER FUNCTIONS
+function map(array, func)
+    local mapped = {}
+
+    for k, v in pairs(array) do
+        mapped[k] = func(v)
+    end
+
+    return mapped
+end
+
+function filter(array, func)
+    local filtered = {}
+
+    for k, v in pairs(array) do
+        if func(v) then
+            table.insert(filtered, v)
+        end
+    end
+
+    return filtered
+end
+
+function split(s, delim)
+    local fields = {}
+    local pattern = string.format('([^%s]+)', delim)
+
+    s:gsub(pattern, function(c) table.insert(fields, c) end)
+
+    return fields
+end
+
+-- MAIN FUNCIONS
+function handle_file_load()
+    filepath = mp.get_property('path')
+    -- check if filepath includes protocol handler (http, av) which mkvmerge cannot handle
+    if string.find(filepath, '://') == nil then
+        mp.unobserve_property(handle_vid_change)
+        mp.observe_property('vid', 'number', handle_vid_change)
+    end
+end
+
+function handle_vid_change()
+    local vid = mp.get_property_number('vid')
+
+    if vid ~= nil then
+        local cropping = get_cropping(filepath, vid)
+        if cropping ~= nil then
+            if options.dynamic_hwdec == true then
+                mp.set_property('hwdec', 'none')
+            end
+            crop_filter = string.format('@matroska-crop:crop=w=in_w-%d:h=in_h-%d:x=%d:y=%d', cropping['left'] + cropping['right'], cropping['top'] + cropping['bottom'], cropping['left'], cropping['top'])
+            mp.command_native({'vf', 'add', crop_filter})
+        else
+            if options.dynamic_hwdec == true then
+                mp.command_native({'vf', 'clr', ''})
+                mp.set_property('hwdec', default_hwdec)
+            end
+        end
+    end
+end
+
+function get_cropping(filepath, vid)
+    -- not using mp.command_native, because it does not support capturing stdout in mpv from debian testing (as of writing this)
+    local mkvmerge_run = utils.subprocess({args={'mkvmerge', '-J', filepath}})
+    if mkvmerge_run.err ~= nil then
+        msg.error(mkvmerge_run.err)
+        return nil
+    end
+
+    local data, err = utils.parse_json(mkvmerge_run.stdout)
+
+    if err ~= nil then
+        msg.error(err)
+        return nil
+    end
+
+    local tracks = data.tracks
+    local video_tracks = filter(data.tracks, function(track) return track.type == 'video' end)
+    local video_track = video_tracks[vid].properties
+
+    if video_track.cropping == nil then
+        return nil
+    end
+
+    local cropping = map(split(video_track.cropping, ','), tonumber)
+
+    return {
+        ['left'] = cropping[1],
+        ['top'] = cropping[2],
+        ['right'] = cropping[3],
+        ['bottom'] = cropping[4],
+    }
+end
+
+options = {
+    dynamic_hwdec = false,
+}
+read_options(options)
+
+if options.dynamic_hwdec then
+    default_hwdec = mp.get_property('hwdec')
+    mp.register_event('file-loaded', handle_file_load)
+else
+    if mp.get_property('hwdec') == 'none' then
+        mp.register_event('file-loaded', handle_file_load)
+    end
+end
